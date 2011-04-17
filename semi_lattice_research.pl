f94752f1836bd0c673f35fd82ff02a7553531697
garmonbozia <lykish@gmail.com>
2011-04-18 03:36:40 +0400

    presentation tex file added

--- semi_lattice_research.pl
+++ semi_lattice_research.pl
@@ -1,64 +1,63 @@
-sublist([], []).
-sublist([H | Tail1], [H | Tail2]) :-
-	sublist(Tail1, Tail2).
-sublist(H, [_ | Tail]) :-
-	sublist(H, Tail).
+sublist( [], [] ).
+sublist( [H | Tail1], [H | Tail2] ) :-
+	sublist( Tail1, Tail2 ).
+sublist( H, [_ | Tail] ) :-
+	sublist( H, Tail ).
 
-less(X, X, _).
-less(X, Z, RelationList) :- 
-	member([X,Z], RelationList).
-less(X, Z, RelationList) :- 
-	member([X,Y], RelationList),
-	less(Y, Z, RelationList),
-	\+less(Z, X, RelationList).
+less( X, X, _ ).
+less( X, Z, RelationList ) :- 
+	member( [X,Z], RelationList ).
+less( X, Z, RelationList ) :- 
+	member( [X,Y], RelationList ),
+	less( Y, Z, RelationList ),
+	\+less( Z, X, RelationList ).
 
-lessList(X, LessList, RelationList) :-
-	findall(Y, less(X, Y, RelationList), List),
-	list_to_set(List, L),
-	sort(L, LessList), !.
-
-bigList(X, LessList, RelationList) :-
-	findall(Y, less(Y, X, RelationList), List),
-	list_to_set(List, L),
-	sort(L, LessList), !.
-	
+lessList( X, LessList, RelationList ) :-
+	findall( Y
+           , less( X, Y, RelationList )
+           , List
+           ),
+	list_to_set( List, L ),
+	sort( L, LessList ),
+    !.
 
 /****************************************
  * L - set of X's elements 
  ****************************************/
-get_set([], [], _).
-get_set([X | Xs], [H | Tail], RelationList) :-
-	lessList(H, List, RelationList),
-	member(X, List),
-	get_set(Xs, Tail, RelationList).	
+get_set( [], [], _ ).
+get_set( [X | Xs], [H | Tail], RelationList ) :-
+	lessList( H, List, RelationList ),
+	member( X, List ),
+	get_set( Xs, Tail, RelationList ).	
 
 /****************************************
  * Y = alpha(X). Eq - permutation string 
  ****************************************/
-alpha(Eq, X, Y) :-
-	nth1(X, Eq, Y).
+alpha( Eq, X, Y ) :- nth1( X, Eq, Y ).
 
 /****************************************
  * alpha : Isotone property
  ****************************************/
-alpha_isotone(Eq, Points, RelationList) :-
-	forall((member(X, Points),
-	member(Y, Points),
-	less(X, Y, RelationList),
-	alpha(Eq, X, X_alpha),
-	alpha(Eq, Y, Y_alpha)),
-	less(X_alpha, Y_alpha, RelationList)).
+alpha_isotone( Eq, Points, RelationList ) :-
+	forall( ( member( X, Points )
+            , member( Y, Points )
+            , less( X, Y, RelationList )
+            , alpha( Eq, X, X_alpha )
+            , alpha( Eq, Y, Y_alpha )
+            )
+          , less( X_alpha, Y_alpha, RelationList )
+          ).
 
 /****************************************
  * alpha^2 = alpha
  ****************************************/
-idempotence(Eq) :- idempotence(Eq, Eq).
-idempotence([], _).
-idempotence([H | Tail], Eq) :-
-	alpha(Eq, H, X),
-	alpha(Eq, X, Y),
+idempotence( Eq ) :- idempotence( Eq, Eq ).
+idempotence( [], _ ).
+idempotence( [H | Tail], Eq ) :-
+	alpha( Eq, H, X ),
+	alpha( Eq, X, Y ),
 	H =:= Y,
-	idempotence(Tail, Eq). 
+	idempotence( Tail, Eq ). 
 
 %% alpha_condition( +Eq, +Elements, +RelationList )
 %
@@ -88,31 +87,31 @@ transitive( PhiList, Elements, RelationList ) :-
           , member( Alpha, PhiList )
 	).
 
-/**********************************/
+list_mltpl( List1, List2, List ) :-
+	findall( X
+           , ( member( X, List1 )
+             , member( X, List2 )
+             )
+           , List ).
 
-list_mltpl(List1, List2, List) :-
-	findall(X, (
-		member(X, List1),
-		member(X, List2)),
-	List).
-
-poset_with_maximum(RelationList, E) :-
-	member(Maximum, E),
-	forall(member(Element, E),
-	less(Maximum, Element, RelationList)),
+poset_with_maximum( RelationList, E ) :-
+	member( Maximum, E ),
+	forall( member( Element, E )
+          , less( Maximum, Element, RelationList )
+          ),
 	!.
 
-chain([_], _).
-chain([H,T | Tail], RelationList) :-
-	less(H, T, RelationList),
-	chain([T|Tail], RelationList),
+chain( [_], _ ).
+chain( [H,T | Tail], RelationList ) :-
+	less( H, T, RelationList ),
+	chain( [T|Tail], RelationList ),
 	!.	
 
-have_inf(X1, X2, RelationList) :-
-	lessList(X1, X1_cone, RelationList),
-	lessList(X2, X2_cone, RelationList),
-	list_mltpl(X1_cone, X2_cone, Cone),
-	poset_with_maximum(RelationList, Cone),
+have_inf( X1, X2, RelationList ) :-
+	lessList( X1, X1_cone, RelationList ),
+	lessList( X2, X2_cone, RelationList ),
+	list_mltpl( X1_cone, X2_cone, Cone ),
+	poset_with_maximum( RelationList, Cone ),
 	!.
 
 %% active_elements( +List, +Elements )
@@ -173,13 +172,13 @@ connective_graph( RelationList, Elements ) :-
 	      , path( X,Y,RelationList,_ )
           ).
 
-semilattice(RelationList, E) :-
-	forall(
-	(	
-		member(X1, E),
-		member(X2, E),
-		X1 < X2),
-	have_inf(X1, X2, RelationList)).
+semilattice( RelationList, E ) :-
+	forall( ( member( X1, E )
+            , member( X2, E )
+            , X1 < X2
+            )
+          ,	have_inf(X1, X2, RelationList)
+          ).
 
 %% phi_for_List( +RelationList, +E, -PhiList )
 %
@@ -219,14 +218,16 @@ poset_with_semilattices( RelationList, E ) :-
             )
           ).
 
-number_of_N_sets(N, Number) :-
-	numlist(1, N, E),
-	findall(List,(
-		relations(List, E),
-		connective_graph(List, E),
-		poset_with_semilattices(List, E)
-	), AllSets),
-	length(AllSets, Number).
+number_of_N_sets( N, Number ) :-
+	numlist( 1, N, E ),
+	findall( List
+           , ( relations( List, E )
+             , connective_graph( List, E )
+             , poset_with_semilattices( List, E )
+             )
+           , AllSets 
+           ),
+	length( AllSets, Number ).
 
 main( N ) :-
     numlist( 1, N, E ),
